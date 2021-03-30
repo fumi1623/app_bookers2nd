b5e8e6c5ed536bec42e9684df19b0cae627e010c
EC2 Default User <ec2-user@ip-172-31-28-56.ec2.internal>
2021-03-30 15:41:04 +0000

    [Start]bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.page(params[:page]).reverse_order
+  end
+
+  def show
+    @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '768e49d795262ff8c7d5c38f6d6146cd0155abea34fc06742cb19562ee434ba9befa72c11a79cfcffd62e528ac782457674a3923bd51943bdd1f67ecca550127'
